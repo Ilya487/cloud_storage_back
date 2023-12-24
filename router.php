1bdbf9c9c6417bc193a76762a45676b340ae6ab7
Ilya <poctadladota@gmail.com>
2023-12-24 23:20:13 +0300

    create router

---
+++ router.php
@@ -0,0 +1,37 @@
+<?php
+spl_autoload_register();
+
+class Router
+{
+    private const CONTROLLERS_NAMESPACE = 'App\Controllers\\';
+
+    private static $routes = [
+        [
+            'path' => '/\/signin$/',
+            'controller' => 'ExampleController'
+        ],
+    ];
+
+    public static function resolvePath()
+    {
+        $requestUrl = $_SERVER['REQUEST_URI'];
+
+        foreach (self::$routes as $route) {
+            $cmpRes = preg_match($route['path'], $requestUrl);
+
+            if ($cmpRes) {
+                $classname = self::CONTROLLERS_NAMESPACE . $route['controller'];
+                $controller = new $classname;
+                self::callController($controller);
+                break;
+            }
+        }
+    }
+
+    private static function callController(\App\Contracts\Controller $controller)
+    {
+        $controller->resolve();
+    }
+}
+
+Router::resolvePath();
