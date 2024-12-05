5955aadfb497c653592f51d00a0d8f41aae661b8
Ilya <poctadladota@gmail.com>
2024-12-05 18:31:44 +0900

    добавил генератор запроса обновления по айди

--- App/Tools/QueryBuilder.php
+++ App/Tools/QueryBuilder.php
@@ -4,17 +4,30 @@ namespace App\Tools;
 
 class QueryBuilder
 {
-    public static function buildInsertQuery(array $fields, $tableName)
+    public static function buildUpdateByIdQuery(array $fields, string $tableName)
     {
-        $preparedParams = self::getPreparedParams($fields);
+        $values = self::getPreparedParams($fields, true);
 
-        return 'INSERT INTO ' . $tableName . ' (' . implode(', ', $fields) . ') VALUES (' . implode(', ', $preparedParams) . ')';
+        return 'UPDATE ' . $tableName . ' SET ' . $values . ' WHERE id=:id';
     }
 
-    private static function getPreparedParams($fields)
+    public static function buildInsertQuery(array $fields, string $tableName)
     {
-        return array_map(function ($val) {
-            return ':' . $val;
+        $preparedParams = self::getPreparedParams($fields, false);
+
+        return 'INSERT INTO ' . $tableName . ' (' . implode(', ', $fields) . ') VALUES (' . $preparedParams . ')';
+    }
+
+    private static function getPreparedParams(array $fields, bool $withNames)
+    {
+        $tmp =  array_map(function ($field) use ($withNames) {
+            if ($withNames) {
+                return  $field . '=:' . $field;
+            }
+
+            return ':' . $field;
         }, $fields);
+
+        return implode(', ', $tmp);
     }
 }
