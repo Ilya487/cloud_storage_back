21606fedb1a18043020b4d843204e41573376e73
Ilya <poctadladota@gmail.com>
2024-12-30 19:33:47 +0900

    написал контроллер отвечающий за регистрацию

--- App/Contracts/Controller.php
+++ App/Contracts/Controller.php
@@ -2,27 +2,7 @@
 
 namespace App\Contracts;
 
-abstract class Controller
+interface Controller
 {
-    protected const CONTENT_TYPE_JSON = 'Content-type: application/json; charset=utf-8';
-
-    final public function __construct()
-    {
-        header("Access-Control-Allow-Origin:" . $_SERVER['HTTP_ORIGIN']);
-    }
-
-    protected function sendAnswer(int $code, array $data = [])
-    {
-        header(self::CONTENT_TYPE_JSON);
-        http_response_code($code);
-
-        $json = json_encode([
-            'code' => $code,
-            ...$data
-        ]);
-
-        echo $json;
-    }
-
-    abstract public function resolve(): void;
+    public function resolve(): void;
 }
