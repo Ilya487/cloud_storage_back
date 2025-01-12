e1cdd6f5eb41007bcd7a3ff6f890837b9009d1d2
Ilya <poctadladota@gmail.com>
2025-01-12 18:38:25 +0900

    изменил структуру middleware

--- App/Http/Middleware/GuestMiddleware.php
+++ App/Http/Middleware/GuestMiddleware.php
@@ -8,12 +8,12 @@ use App\Http\Response;
 
 class GuestMiddleware implements MiddlewareInterface
 {
-    public function __construct(private AuthenticationInterface $authService) {}
+    public function __construct(private Response $response, private AuthenticationInterface $authService) {}
 
-    public function handle(Request $request, Response $response)
+    public function handle()
     {
         if ($this->authService->auth()) {
-            $response->setStatusCode(400)->sendJson(['code' => 400, 'message' => 'Вы уже авторизованы']);
+            $this->response->setStatusCode(400)->sendJson(['code' => 400, 'message' => 'Вы уже авторизованы']);
         }
     }
 }
