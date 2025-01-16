294132512975c0f483f6f27a618bf71bd6b465c8
Ilya <poctadladota@gmail.com>
2025-01-17 02:04:36 +0900

    создал репозиторий для работы с файловой системой

---
+++ App/Repositories/FileSystemRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Tools\DbConnect;
+use App\Tools\QueryBuilder;
+use PDO;
+
+class FileSystemRepository
+{
+    private PDO $pdo;
+    private QueryBuilder $queryBuilder;
+
+
+    public function __construct(DbConnect $dbConnect)
+    {
+        $this->pdo = $dbConnect->getConnection();
+        $this->queryBuilder = new QueryBuilder('file_system');
+    }
+
+    /**
+     * @return string new dir id
+     */
+    public function createDir(string $userId, string $dirName, string $path, string $parentDirId = null): string
+    {
+        $query = $this->queryBuilder->insert(['name', 'user_id', 'created_at', 'parent_id', 'type', 'path'])->build();
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute([
+            'name' => $dirName,
+            'user_id' => $userId,
+            'created_at' => date('Y-m-d'),
+            'parent_id' => $parentDirId,
+            'type' => 'folder',
+            'path' => $path
+        ]);
+
+        return $this->pdo->lastInsertId();
+    }
+
+    public function getDirPath(string $dirId): ?string
+    {
+        $query = $this->queryBuilder->select(['path'])->where('id', QueryBuilder::EQUAL)->build();
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute(['id' => $dirId]);
+        return $stmt->fetch(PDO::FETCH_ASSOC)['path'];
+    }
+}
