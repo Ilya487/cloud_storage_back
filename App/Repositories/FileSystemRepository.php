3c95ec595bcfed76048f393217b41231f58e5bf2
Ilya <poctadladota@gmail.com>
2025-02-06 02:36:48 +0900

    Добавил в fsRepository метод для удаления сущности + добавилл соответствующий метод в fsService

--- App/Repositories/FileSystemRepository.php
+++ App/Repositories/FileSystemRepository.php
@@ -25,10 +25,10 @@ class FileSystemRepository extends BaseRepository
         return $newDirId;
     }
 
-    public function getDirPathById(int $dirId): null|string|false
+    public function getPathById(int $id): null|string|false
     {
         $query = $this->queryBuilder->select(['path'])->where('id', QueryBuilder::EQUAL)->build();
-        $data = $this->fetchOne($query, ['id' => $dirId]);
+        $data = $this->fetchOne($query, ['id' => $id]);
 
         if ($data === false) return false;
         else return $data['path'];
@@ -52,7 +52,13 @@ class FileSystemRepository extends BaseRepository
         else return $this->getConcreteDirContent($userId, $dirId);
     }
 
-    private function getRootContent(string $userId): array|false
+    public function deleteById(int $userId, int $itemId)
+    {
+        $query = $this->queryBuilder->delete()->where('id', QueryBuilder::EQUAL)->and('user_id', QueryBuilder::EQUAL)->build();
+        $this->delete($query, ['id' => $itemId, 'user_id' => $userId]);
+    }
+
+    private function getRootContent(int $userId): array|false
     {
         $query = $this->queryBuilder->select()->where('user_id', QueryBuilder::EQUAL)->and('parent_id', QueryBuilder::IS_NULL)->build();
         $content = $this->fetchAll($query, ['user_id' => $userId]);
--- App/Services/FileSystemService.php
+++ App/Services/FileSystemService.php
@@ -12,7 +12,7 @@ class FileSystemService
 
     public function createFolder(int $userId, string $dirName, ?int $parentDirId = null): ?OperationResult
     {
-        $parentPath = $parentDirId ? $this->fsRepo->getDirPathById($parentDirId) : '';
+        $parentPath = $parentDirId ? $this->fsRepo->getPathById($parentDirId) : '';
         if ($parentPath === false) return new OperationResult(false, null, ['message' => 'Неверный айди родительского каталога']);
 
         if ($this->diskStorage->createDir($userId, $dirName, $parentPath)) {
@@ -24,7 +24,9 @@ class FileSystemService
 
     public function getFolderContent(int $userId, ?int $dirId = null): ?OperationResult
     {
-        $pathToSelectedDir = is_null($dirId) ? '/' : $this->fsRepo->getDirPathById($dirId);
+        $pathToSelectedDir = is_null($dirId) ? '/' : $this->fsRepo->getPathById($dirId);
+        if ($pathToSelectedDir === false) return new OperationResult(false, null, ['message' => 'Указан неверный айди']);
+
         $catalogData = $this->fsRepo->getDirContent($userId, $dirId);
 
         if ($catalogData !== false) return new OperationResult(true, ['path' => $pathToSelectedDir, 'contents' => $catalogData]);
@@ -38,7 +40,9 @@ class FileSystemService
 
     public function renameFolder(int $userId, int $dirId, string $newName)
     {
-        $folderPath = $this->fsRepo->getDirPathById($dirId);
+        $folderPath = $this->fsRepo->getPathById($dirId);
+        if ($folderPath === false) return new OperationResult(false, null, ['message' => 'Указан неверный айди']);
+
         if ($this->diskStorage->renameDir($userId, $newName, $folderPath)) {
             $lastSlashPos = strrpos($folderPath, '/', -1);
             $updatedPath = substr($folderPath, 0, $lastSlashPos + 1) . $newName;
@@ -47,6 +51,19 @@ class FileSystemService
             return new OperationResult(true, ['updatedPath' => $updatedPath]);
         } else {
             return new OperationResult(false, null, ['error' => 'Не удалось переименовать папку']);
+        }
+    }
+
+    public function deleteFolder(int $userId, int $dirId)
+    {
+        $folderPath = $this->fsRepo->getPathById($dirId);
+        if ($folderPath === false) return new OperationResult(false, null, ['message' => 'Указан неверный айди']);
+
+        if ($this->diskStorage->deleteDir($userId, $folderPath)) {
+            $this->fsRepo->deleteById($userId, $dirId);
+            return new OperationResult(true, ['message' => 'Папка успешно удалена']);
+        } else {
+            return new OperationResult(false, null, ['message' => 'Не удалось удалить папку']);
         }
     }
 }
