80cb4cc6a6af740ea5f4180909bf2c4739805bab
ilya <poctadladota@gmail.com>
2026-02-28 20:18:34 +0900

    Refactor buildArchiveForDownload to accept FileSystemObjectCollection and enhance duplicate handling

--- App/Services/DownloadArchiveService.php
+++ App/Services/DownloadArchiveService.php
@@ -2,11 +2,12 @@
 
 namespace App\Services;
 
-use App\Models\FileSystemObject;
+use App\Models\Collections\FileSystemObjectCollection;
 use App\Storage\DiskStorage;
 use App\Storage\DownloadStorage;
 use App\Tools\ArchiveException;
-use Exception;
+use ReflectionProperty;
+use SplObjectStorage;
 
 class DownloadArchiveService
 {
@@ -15,16 +16,11 @@ class DownloadArchiveService
         private DiskStorage $diskStorage,
     ) {}
 
-    /**
-     * @param array<FileSystemObject> $files
-     */
-    function buildArchiveForDownload(int $downloadId, array $files): FilesPrepareResult
+    function buildArchiveForDownload(int $downloadId, FileSystemObjectCollection $files): FilesPrepareResult
     {
-        $this->checkTypes($files);
-
         $archive = $this->downloadStorage->createArchive(
             $downloadId,
-            count($files) == 1 ? $files[0]->getName() : ''
+            $files->len() == 1 ? $files[0]->getName() : ''
         );
 
         if ($archive === false) {
@@ -34,14 +30,26 @@ class DownloadArchiveService
         $successAdded = [];
         $errorAdded = [];
 
+        $files = $this->resolveDuplicates($files);
+
         foreach ($files as $file) {
-            $fullPath = $this->diskStorage->getPath($file->ownerId, $file->getPath());
+            if (!$file->isFile()) {
+                $addRes = $archive->createDir($file->getPath());
+                if ($addRes === false) {
+                    $errorAdded[] = $file;
+                    continue;
+                }
+                $successAdded[] = $file;
+                continue;
+            }
+
+            $fullPath = $this->diskStorage->getPath($file->id, $file->getExt());
             if ($fullPath === false) {
                 $errorAdded[] = $file;
                 continue;
             }
 
-            $addRes = $archive->add($fullPath);
+            $addRes = $archive->add($fullPath, $file->getPath());
             if ($addRes === false) $errorAdded[] = $file;
             else $successAdded[] = $file;
         }
@@ -57,12 +65,55 @@ class DownloadArchiveService
         }
     }
 
-    private function checkTypes(array $files)
+    private function resolveDuplicates(FileSystemObjectCollection $files)
     {
-        foreach ($files as $file) {
-            if (!($file instanceof FileSystemObject))
-                throw new Exception($file . ' is not instance of FileSystemObject');
+        while ($this->isNeedToRename($files)) {
+            $pathMap = [];
+            $renameQueue = new SplObjectStorage;
+
+            foreach ($files as $file) {
+                if (!isset($pathMap[$file->getPath()])) {
+                    $pathMap[$file->getPath()] = 1;
+                    continue;
+                }
+
+                $count = ++$pathMap[$file->getPath()];
+                $currentPath = $file->getPath();
+
+                $updatedName = $file->getName() . "($count)" . ($file->getExt() ? ".{$file->getExt()}" : '');
+                $file->rename($updatedName);
+                if ($file->isFile()) continue;
+
+                $renameQueue[$file] = $currentPath;
+            }
+
+            foreach ($renameQueue as $file) {
+                $oldPath = $renameQueue[$file];
+
+                foreach ($files->getFolderContent($file->getPathIds()) as $subFile) {
+                    $path = $subFile->getPath();
+                    $path = str_replace($oldPath, $file->getPath(), $path);
+                    new ReflectionProperty($subFile, 'path')->setValue($subFile, $path);
+                }
+            }
         }
+
+
+        return $files;
+    }
+
+    private function isNeedToRename(FileSystemObjectCollection $files): bool
+    {
+        $pathMap = [];
+        foreach ($files as $file) {
+            if (!isset($pathMap[$file->getPath()])) {
+                $pathMap[$file->getPath()] = 1;
+                continue;
+            }
+            return true;
+        }
+
+        return false;
     }
 }
 
